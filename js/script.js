cc0e2377c95458426c4ee160dad370274e9ee1af
Marcos Santos <santos.marcos.programador@gmail.com>
2023-12-28 19:20:44 -0300

    Validação do campos!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-let title = `Calculadora de Juros e Descontos`;
-let value = parseFloat(prompt(`${title}\n\nInforme o valor: `));
-let percentage = parseFloat(prompt(`${title}\n\nInforme a por porcentagem: `));
+let run = true,
+  title = `Calculadora de Juros e Descontos`,
+  value,
+  percentage;
 
-let percentageValue = value * (percentage / 100);
+do {
+  do {
+    value = parseFloat(prompt(`${title}\n\nInforme o valor: `));
+  } while (isNaN(value) || value < 0);
+  do {
+    percentage = parseFloat(prompt(`${title}\n\nInforme a por porcentagem: `));
+  } while (isNaN(percentage) || percentage < 0);
 
-let discount = value - percentageValue;
-let interest = value + percentageValue;
+  let percentageValue = value * (percentage / 100);
+  let discount = value - percentageValue;
+  let interest = value + percentageValue;
 
-alert(
-  `${title}\n\nValor: ${value.toFixed(2)}\nPorcentagem: ${percentage.toFixed(
-    2
-  )}\nValor da Porcentagem: ${percentageValue.toFixed(
-    2
-  )}\nDesconto: ${discount.toFixed(2)}\nJuros: ${interest.toFixed(2)}`
-);
+  alert(
+    `${title}\n\nValor: ${value.toFixed(2)}\nPorcentagem: ${percentage.toFixed(
+      2
+    )}\nValor da Porcentagem: ${percentageValue.toFixed(
+      2
+    )}\nDesconto: ${discount.toFixed(2)}\nJuros: ${interest.toFixed(2)}`
+  );
+  run = confirm(`${title}\n\nDeseja realizar outro cálculo?`);
+} while (run);
